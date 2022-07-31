4ee6aeed122b0fc911d20116d97f2e8e22f660b1
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-31 14:14:10 -0300

    Loading with Spinner

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
 import { api } from "../../api/api";
 import { CharacterModel } from "../../models/CharacterModel";
 import { CharacterCard } from "../CharacterCard/CharacterCard";
@@ -8,21 +9,29 @@ import { Container } from "./styles";
 export function CharacterList() {
   const [characterList, setCharacterList] = useState<Array<CharacterModel>>([]);
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchCharacterList();
   }, [page]);
 
   async function fetchCharacterList() {
-    const { results } = await api.getCharacters(page);
-    setCharacterList(results);
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    try {
+      setIsLoading(true);
+      const { results } = await api.getCharacters(page);
+      setCharacterList(results);
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
     <Container>
       <h1>Lista de Personagens</h1>
-
+      {isLoading && <Spinner animation="border" />}
       {characterList?.map((character) => (
         <CharacterCard character={character} />
       ))}
