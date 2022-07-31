27be30cd7d289e8afa61dc7857342c13f8a55d0e
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-31 14:27:38 -0300

    fech with useQuery

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,38 +1,24 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import Spinner from "react-bootstrap/Spinner";
 import { api } from "../../api/api";
-import { CharacterModel } from "../../models/CharacterModel";
+
 import { CharacterCard } from "../CharacterCard/CharacterCard";
 import { Pagination } from "../Pagination/Pagination";
 import { Container } from "./styles";
 
 export function CharacterList() {
-  const [characterList, setCharacterList] = useState<Array<CharacterModel>>([]);
   const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    fetchCharacterList();
-  }, [page]);
-
-  async function fetchCharacterList() {
-    try {
-      setIsLoading(true);
-      const { results } = await api.getCharacters(page);
-      setCharacterList(results);
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  const { data, isLoading } = useQuery(["characters", page], () =>
+    api.getCharacters(page)
+  );
 
   return (
     <Container>
       <h1>Lista de Personagens</h1>
       {isLoading && <Spinner animation="border" />}
-      {characterList?.map((character) => (
+      {data?.results.map((character) => (
         <CharacterCard character={character} />
       ))}
       <Pagination
