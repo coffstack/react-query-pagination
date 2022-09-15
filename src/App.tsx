b4f660839abd54dc77e22f3655ac8495dd70e271
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-09-15 13:27:12 -0300

    config react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 
 import "./App.css";
 import { CharacterList } from "./components/CharacterList/CharacterList";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <CharacterList />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <CharacterList />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
