c1eb9713159ee6c273191a1ca2006036f2c3bb40
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-09-15 09:32:56 -0300

    update pagination

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,39 +1,49 @@
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 import Spinner from "react-bootstrap/Spinner";
 import { api } from "../../api/api";
 
 import { CharacterCard } from "../CharacterCard/CharacterCard";
-import { Pagination } from "../Pagination/Pagination";
-import { Container } from "./styles";
+import { Container, CurrentPageText, PaginationBox } from "./styles";
 
 export function CharacterList() {
   const [page, setPage] = useState(1);
 
-  const { data, isLoading } = useQuery(
+  const { data, isLoading, isPreviousData } = useQuery(
     ["characters", page],
     () => api.getCharacters(page),
     {
       keepPreviousData: true,
-      onSettled: () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      },
+      staleTime: 5000,
     }
   );
 
   return (
     <Container>
       <h1>Lista de Personagens</h1>
+      <PaginationBox>
+        <Button
+          onClick={() => setPage((old) => Math.max(old - 1, 1))}
+          disabled={page === 1}
+        >
+          Página Anterior
+        </Button>
+        <CurrentPageText>Página Atual: {page}</CurrentPageText>
+        <Button
+          onClick={() => {
+            setPage((old) => (data?.info.next ? old + 1 : old));
+          }}
+          disabled={isPreviousData || !data?.info.next}
+        >
+          Próxima Página
+        </Button>
+      </PaginationBox>
+
       {isLoading && <Spinner animation="border" />}
       {data?.results.map((character) => (
-        <CharacterCard character={character} />
+        <CharacterCard key={character.id} character={character} />
       ))}
-      <Pagination
-        onClick={setPage}
-        start={page - 4 > 0 ? page - 4 : 1}
-        active={page}
-        end={page + 4}
-      />
     </Container>
   );
 }
--- src/components/CharacterList/styles.ts
+++ src/components/CharacterList/styles.ts
@@ -5,3 +5,15 @@ export const Container = styled.div`
   align-items: center;
   flex-direction: column;
 `;
+
+export const PaginationBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 16px;
+`;
+export const CurrentPageText = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  padding-right: 16px;
+  padding-left: 16px;
+`;
