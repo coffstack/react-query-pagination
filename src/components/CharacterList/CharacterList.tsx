905601100067461526ead4e7a765df4438f52358
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-09-15 12:35:55 -0300

    remove page

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -2,19 +2,18 @@ import { useEffect, useState } from "react";
 import { api } from "../../api/api";
 import { CharacterModel } from "../../models/CharacterModel";
 import { CharacterCard } from "../CharacterCard/CharacterCard";
-import { Pagination } from "../Pagination/Pagination";
+
 import { Container } from "./styles";
 
 export function CharacterList() {
   const [characterList, setCharacterList] = useState<Array<CharacterModel>>([]);
-  const [page, setPage] = useState(1);
 
   useEffect(() => {
     fetchCharacterList();
-  }, [page]);
+  }, []);
 
   async function fetchCharacterList() {
-    const { results } = await api.getCharacters(page);
+    const { results } = await api.getCharacters(1);
     setCharacterList(results);
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
@@ -26,12 +25,6 @@ export function CharacterList() {
       {characterList?.map((character) => (
         <CharacterCard character={character} />
       ))}
-      <Pagination
-        onClick={setPage}
-        start={page - 4 > 0 ? page - 4 : 1}
-        active={page}
-        end={page + 4}
-      />
     </Container>
   );
 }
